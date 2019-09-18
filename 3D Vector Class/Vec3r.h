2ca30ff126bcc7c45c9d7f6af39dad538114f78b
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-09-18 16:20:11 +0200

    Operators done (not ==)

--- 3D Vector Class/Vec3r.h
+++ 3D Vector Class/Vec3r.h
@@ -1,14 +1,53 @@
 #pragma once
 
+class Vec3d {
 
-template <class T>
-
-class Vec3 {
-
-private:
+public:
 	int x;
 	int y;
 	int z;
 
+	Vec3d() { x = 0; y = 0; z = 0; };
+	Vec3d(int x1, int y1, int z1) { x=x1; y=y1; z=z1; };
+	Vec3d(const Vec3d &vector);
 
+
+	Vec3d operator+(const Vec3d &vector)
+	{
+		return Vec3d(x + vector.x, y + vector.y, z + vector.z);
+	}
+
+	Vec3d operator-(const Vec3d &vector)
+	{
+		return Vec3d(x - vector.x, y - vector.y, z - vector.z);
+	}
+
+	Vec3d operator+=(const Vec3d &vector)
+	{
+		x += vector.x;
+		y += vector.y;
+		z += vector.z;
+		return *this;
+	}
+
+	Vec3d operator-=(const Vec3d &vector)
+	{
+		x -= vector.x;
+		y -= vector.y;
+		z -= vector.z;
+		return *this;
+	}
+
+	Vec3d operator=(const Vec3d &vector)
+	{
+		x = vector.x;
+		y = vector.y;
+		z = vector.z;
+		return *this;
+	}
+
+	Vec3d operator==(const Vec3d &vector) {
+
+
+	}
 };
