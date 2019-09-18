14d0eb03f85c446f1be620b2661ab3db77dfe3e8
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-09-18 20:54:51 +0200

    Amost done (distance no working and = sign either)

--- 3D Vector Class/Main.cpp
+++ 3D Vector Class/Main.cpp
@@ -3,8 +3,18 @@
 
 int main() {
 
-	Vec3d u = Vec3d(2, 5, 7);
+	Vec3d u = Vec3d(1, 1, 1);
 	Vec3d v = Vec3d(3, 7, 10);
+
+	Vec3d w = u + v;
+	Vec3d a = u - v;
+	Vec3d b = u += v;
+	Vec3d c = u -= v;
+
+	w.print();
+	a.print();
+	b.print();
+	c.print();
 
 	system("pause");
 	return 0;
--- 3D Vector Class/Vec3r.h
+++ 3D Vector Class/Vec3r.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 class Vec3d {
 
@@ -10,7 +11,6 @@ public:
 	Vec3d() { x = 0; y = 0; z = 0; };
 	Vec3d(int x1, int y1, int z1) { x=x1; y=y1; z=z1; };
 	Vec3d(const Vec3d &vector);
-
 
 	Vec3d operator+(const Vec3d &vector)
 	{
@@ -24,21 +24,15 @@ public:
 
 	Vec3d operator+=(const Vec3d &vector)
 	{
-		x += vector.x;
-		y += vector.y;
-		z += vector.z;
-		return *this;
+		return Vec3d(x += vector.x, y += vector.y, z += vector.z);
 	}
 
 	Vec3d operator-=(const Vec3d &vector)
 	{
-		x -= vector.x;
-		y -= vector.y;
-		z -= vector.z;
-		return *this;
+		return Vec3d(x -= vector.x, y -= vector.y, z -= vector.z);
 	}
 
-	Vec3d operator=(const Vec3d &vector)
+	Vec3d operator=(const Vec3d &vector) 
 	{
 		x = vector.x;
 		y = vector.y;
@@ -46,8 +40,65 @@ public:
 		return *this;
 	}
 
-	Vec3d operator==(const Vec3d &vector) {
+	bool operator==(const Vec3d &vector) 
+	{
+
+		if (x == vector.x && y == vector.y && z == vector.z) {
+
+			return true;
+		}
+		else {
+
+			return false;
+		}
+	}
+
+	Vec3d operator/(int value)
+	{
+		x /= value;
+		y /= value;
+		z /= value;
+		return *this;
+	}
 
 
+	void Zero() {
+
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	Vec3d normalization()
+	{
+		
+		*this / v_magnitude();
+		return *this;
+	}
+
+	bool is_Zero() const{ 
+
+		return x == 0 && y == 0 && z == 0;
+	}
+
+	int v_square()
+	{
+		return x * x + y * y + z * z;
+	}
+
+	int v_magnitude()
+	{
+		return sqrt(v_square());
+	}
+
+	int distance_to(const Vec3d &vector)
+	{
+		Vec3d dist = *this - vector;
+		return dist.v_magnitude;
+	}
+
+	void print()
+	{
+		std::cout << x << " " << y << " " << z << std::endl;
 	}
 };
