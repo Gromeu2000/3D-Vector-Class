536513644bd6ff0bf1e0de4eb76f760c8663bc2d
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-09-23 12:10:29 +0200

    Done

--- 3D Vector Class/Vec3r.h
+++ 3D Vector Class/Vec3r.h
@@ -1,104 +1,110 @@
-#pragma once
+#ifndef _HEADER_H
+
+#include <stdio.h>
+#include <locale.h>
 #include <iostream>
+using namespace std;
 
-class Vec3d {
+template <class T>
 
+
+class vec3
+{
 public:
-	int x;
-	int y;
-	int z;
 
-	Vec3d() { x = 0; y = 0; z = 0; };
-	Vec3d(int x1, int y1, int z1) { x=x1; y=y1; z=z1; };
-	Vec3d(const Vec3d &vector);
+	T x, y, z;
 
-	Vec3d operator+(const Vec3d &vector)
+	vec3(T x1, T y1, T z1)
 	{
-		return Vec3d(x + vector.x, y + vector.y, z + vector.z);
+		x = x1;
+		y = y1;
+		z = z1;
 	}
 
-	Vec3d operator-(const Vec3d &vector)
+	// NORMALIZE
+	void normalize()
 	{
-		return Vec3d(x - vector.x, y - vector.y, z - vector.z);
+		float module;
+		module = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+		x = x / module;
+		y = y / module;
+		z = z / module;
 	}
 
-	Vec3d operator+=(const Vec3d &vector)
+	// CHANGE THE PARAMETERS OF THE VECTOR TO 0
+	void zero()
 	{
-		return Vec3d(x += vector.x, y += vector.y, z += vector.z);
-	}
-
-	Vec3d operator-=(const Vec3d &vector)
-	{
-		return Vec3d(x -= vector.x, y -= vector.y, z -= vector.z);
-	}
-
-	Vec3d operator=(const Vec3d &vector) 
-	{
-		x = vector.x;
-		y = vector.y;
-		z = vector.z;
-		return *this;
-	}
-
-	bool operator==(const Vec3d &vector) 
-	{
-
-		if (x == vector.x && y == vector.y && z == vector.z) {
-
-			return true;
-		}
-		else {
-
-			return false;
-		}
-	}
-
-	Vec3d operator/(int value)
-	{
-		x /= value;
-		y /= value;
-		z /= value;
-		return *this;
-	}
-
-
-	void Zero() {
-
 		x = 0;
 		y = 0;
 		z = 0;
 	}
 
-	Vec3d normalization()
+	// CHECK IF VECTOR IS 0
+	bool is_zero()
 	{
-		
-		*this / v_magnitude();
-		return *this;
+		if (x == 0 && y == 0 && z == 0)
+		{
+			return true;
+		}
+
+		return false;
 	}
 
-	bool is_Zero() const{ 
-
-		return x == 0 && y == 0 && z == 0;
+	// CALCULATE DISTANCE
+	float distance_to(vec3 v1)
+	{
+		float result;
+		result = sqrt(pow((v1.x - x), 2) + pow((v1.y - y), 2) + pow((v1.z - z), 2));
+		return result;
 	}
 
-	int v_square()
+	// ADDITION
+	vec3 operator+(vec3 <float> v)
 	{
-		return x * x + y * y + z * z;
+		vec3 <float> res(0, 0, 0);
+		res.x = this->x + v.x;
+		res.y = this->y + v.y;
+		res.z = this->z + v.z;
+		return res;
 	}
 
-	int v_magnitude()
+	//SUBSTRACTION
+	vec3 operator-(vec3 <float> v)
 	{
-		return sqrt(v_square());
+		vec3 <float> res(0, 0, 0);
+		res.x = this->x - v.x;
+		res.y = this->y - v.y;
+		res.z = this->z - v.z;
+		return res;
 	}
 
-	int distance_to(const Vec3d &vector)
+	vec3 operator+=(vec3 <float> v)
 	{
-		Vec3d dist = *this - vector;
-		return dist.v_magnitude;
+		vec3 <float> res(0, 0, 0);
+		res.x = this->x += v.x;
+		res.y = this->y += v.y;
+		res.z = this->z += v.z;
+		return res;
 	}
 
-	void print()
+	vec3 operator-=(vec3 <float> v)
 	{
-		std::cout << x << " " << y << " " << z << std::endl;
+		vec3 <float> res(0, 0, 0);
+		res.x = this->x -= -v.x;
+		res.y = this->y -= -v.y;
+		res.z = this->z -= -v.z;
+		return res;
+	}
+
+	vec3 operator==(vec3 <T>v1) {
+		{
+			if (this->x == v1->x && this->y == v1->y && this->z == v1->z)
+			{
+				return true;
+			}
+			return false;
+		}
 	}
 };
+
+#endif //_HEADER_H
